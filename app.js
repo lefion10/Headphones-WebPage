d1fa84e4fd8ae791752a0a40d1ef4840f9c65ec4
Puscuta Alexandru-Ionut <74986114+lefion10@users.noreply.github.com>
2021-07-14 15:56:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,5 +5,7 @@ tl.to(".slider" , {y: "-100%" , duration:1.5,delay:0.5});
 tl.to(".intro" , {y: "-100%" , duration:1,} , "-=1");
 tl.fromTo("nav" , {opacity:0} , {opacity:1 , duration:1})
 tl.fromTo(".main-text" , {opacity:0} , {opacity:1 , duration:1} , "-=1");
-
+tl.fromTo(".logo" , {opacity:0} , {opacity:1 , duration:1} , "-=1");
+tl.fromTo(".row" , {opacity:0} , {opacity:1 , duration:1} , "-=1");
+tl.fromTo(".socialmedia" , {opacity:0} , {opacity:1 , duration:1} , "-=1");
 
